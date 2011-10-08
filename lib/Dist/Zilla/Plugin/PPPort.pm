62ce933bb2587c36f4664ac9b5e232c2c29f42c9
Leon Timmermans <fawaka@gmail.com>
2011-10-09 01:27:53 +0200

    Initial implementation

--- lib/Dist/Zilla/Plugin/PPPort.pm
+++ lib/Dist/Zilla/Plugin/PPPort.pm
@@ -1,5 +1,35 @@
 package Dist::Zilla::Plugin::PPPort;
-use strict;
-use warnings;
+
+use 5.008;
+use Moose;
+with qw/Dist::Zilla::Role::FileGatherer/;
+use Devel::PPPort;
+use Symbol;
+
+my $content;
+open PPPORT_FILE, '>', \$content or confess "Couldn't open scalar filehandle";
+Devel::PPPort::WriteFile("&=".__PACKAGE__."::PPPORT_FILE");
+close PPPORT_FILE;
+
+sub gather_files {
+	my $self = shift;
+	my $file = Dist::Zilla::File::InMemory->new(name => 'ppport.h', content => $content);
+	$self->add_file($file);
+	return;
+}
 
 1;
+
+__END__
+
+#ABSTRACT: PPPort for Dist::Zilla
+
+=head1 SYNOPSIS
+
+In your dist.ini
+
+ [PPPort]
+
+=for Pod::Coverage
+gather_files
+=end
